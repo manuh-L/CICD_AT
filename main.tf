8af14fbf99b564f14beaf1ff066f3221b99446a1
manuh-L <manuel.nhiuana@gmail.com>
2020-09-17 21:35:46 +0200

    var keys

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+#  access_key = var.aws_access_key
+#  secret_key = var.aws_secret_key
   region     = var.region
 }
 
