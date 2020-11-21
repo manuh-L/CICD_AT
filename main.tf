3a05aaa6bc3518038f2376ca7548d1be2571c436
manuh-L <manuel.nhiuana@gmail.com>
2020-11-21 20:14:15 +0200

    show variable region

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 provider "aws" {
 #  access_key = var.aws_access_key
 #  secret_key = var.aws_secret_key
-  #region     = var.region
+  region     = var.region
 }
 
 ##################################################################################
