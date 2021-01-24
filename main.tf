aa8c34087e5b9da723c511adb5342b8ee0898f02
manuh-L <manuel.nhiuana@gmail.com>
2021-01-24 09:26:03 +0200

    count

--- main.tf
+++ main.tf
@@ -90,8 +90,8 @@ resource "aws_instance" "apache_terraform" {
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
-      Name = var.tag_Name[count.index]
-      Role = var.tag_Role [count.index]
+      Name = "${var.tag_Name} ${count.index}"
+#            Role = var.tag_Role [count.index]
 
   }
 
@@ -130,6 +130,7 @@ resource "aws_instance" "apache_terraform" {
 #EOF
 #EOD
 #  }
+# "${var.azure_bastionserver}${format("%02d", count.index + 1)}"
 
 
 
